7eaa1ca7d39548584fa57ef80f8de993a015c52c
XueShan <289159468@qq.com>
2017-03-01 23:53:21 +0800

    kline

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import KLine from './lib/kline'
 import Timeseries from './lib/timeseries'
-import KlineData from './mock/kline'
-
-import {calcMA} from './lib/kline-utils'
+import KlineData from './mock/kline-new'
 
 // import io from 'socket.io-client/dist/socket.io'
 
@@ -10,6 +8,21 @@ var root = document.getElementById('root')
 var k = new KLine (root)
 k.setData(KlineData.data)
 k.render()
+
+var testButton = document.createElement('button')
+testButton.innerHTML = 'destroy'
+testButton.style.padding = '10px 20px'
+testButton.onclick = function () {
+    k.destroy()
+}
+var testButton2 = document.createElement('button')
+testButton2.innerHTML = 'change size'
+testButton2.style.padding = '10px 20px'
+testButton2.onclick = function () {
+    k.insertVOL()
+}
+root.appendChild(testButton)
+root.appendChild(testButton2)
 // var socket = io.connect('ws://10.2.237.250:8060')
 // socket.emit('kl', {exchange: 105, code: '000776'})
 // socket.on('kl', data => {
